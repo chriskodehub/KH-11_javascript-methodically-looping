15cb64dc13d1a7666067d3f18c899afc052fa429
chriskodehub <chris.kodehode@gmail.com>
2022-03-01 12:29:51 +0100

    feat: finish oppgave 7 end 8

---
+++ js/oppgave-7.js
@@ -0,0 +1,5 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    words = ["en", "to", "tre", "fire", "fem", "seks", "syv", "åtte", "ni", "ti"];
+
+console.log(numbers.toString());
+console.log(words.toString());
---
+++ js/oppgave-8.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (let i=1;i<numbers.length-2;i++){
+    numbers.fill(13,i,i+1);
+    console.log(numbers);
+}
