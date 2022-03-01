80b9bdfc7e4c808ddbd9092cf3f5e870dd5b88f4
chriskodehub <chris.kodehode@gmail.com>
2022-03-01 06:38:34 +0100

    feat: finish oppgave 5

--- js/oppgave-5.js
+++ js/oppgave-5.js
@@ -1,40 +1,20 @@
-const sampleText =
-    "This i tekst with word if, word as , word *1234 and 1234*, function addBigLetter bigLetterAdd bigAddLetter, case sensitive If";
+let sampleText =
+    "This is text with word if, word as , word *1234 and 1234*, function addBigLetter bigLetterAdd bigAddLetter, case sensitive If";
 
-const wordIf = findWord("if");
-const wordAs = findWord("as");
-const wordAnd = findWord("and");
+const wordIf = replaceWord("if", "hvis");
+const wordAs = replaceWord("as", "som");
+const wordAnd = replaceWord("and", "i");
+const wordWords = replaceWord("words", "ord");
 
-console.log(`${wordIf} | ${wordAs} | ${wordAnd}`);
+console.log(sampleText);
 
-
-function findWord(word) {
-    // Main body of function
-    const regexSearch = new RegExp(`\\b${word}\\b`, "i");
-    const indexOfWord = sampleText.search(regexSearch);
-
-    // Call next function
-    if (indexOfWord === -1) {
-        return "Word was not found";
+function replaceWord(englishWord, norwegianWord) {
+    if (sampleText.search(englishWord) === -1) {
+        console.log(`Word <${englishWord}> was not found`);
     } else {
-        return prepareStringToReplace(indexOfWord, word.length);
+        sampleText = sampleText.replace(englishWord, norwegianWord);
+        console.log(
+            `Word <${englishWord}> was replaced with <${norwegianWord}>`
+        );
     }
-}
-
-function prepareStringToReplace(indexOfSearchText, lengthOfText) {
-    const startPosition = calculateStartPosition(indexOfSearchText);
-    const endPosition = calculateEndPosition(indexOfSearchText + lengthOfText);
-    return sampleText.slice(startPosition, endPosition);
-}
-
-function calculateStartPosition(startIndex) {
-    const newStartIndex = startIndex - 5;
-    if (newStartIndex < 0) return 0;
-    else return newStartIndex;
-}
-
-function calculateEndPosition(endIndex) {
-    const newEndIndex = endIndex + 5;
-    if (newEndIndex >= sampleText.length) return sampleText.length;
-    else return newEndIndex;
 }
